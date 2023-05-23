cf51f0a85b8edba82d735243d4ece9f1fe65c11f
prabhat <pokharelprabhat00@gmail.com>
2023-05-23 13:47:35 +0545

    first chapter

---
+++ switch/ifelse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int number;
+
+  cout << "Enter mark: ";
+  cin >> number;
+
+  
+  if (number > 32) {
+    cout << "You pass the exam .hurray and best of luck for your future .i know you will do it. : " << number << endl;
+
+  }
+  else
+  {
+    cout<<"just sucide bro" << number << endl;
+  }
+
+
+
+  return 0;
+}
---
+++ switch/switchstatement.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,p;
+    cout<<"enter a number:"<<endl;
+    cin>>n;
+    cin>>p;
+    switch(p){
+        case'p':
+        cout<<"number is nuber"<<n<<endl;
+        break;
+        case'2':
+        cout<<n*23<<endl;
+        break;
+        default:
+
+        break;
+        }
+        
+    
+
+}
